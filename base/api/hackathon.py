140b986009aea5a429ed5a8a7edbf36ee284c5e8
0x4ym4n <0x4ym4n@gmail.com>
2022-04-02 11:38:45 +0400

    changes

--- base/api/hackathon.py
+++ base/api/hackathon.py
@@ -182,7 +182,7 @@ def pad(image, token):
         'Authorization': 'JWT ' + token,
         'Content-Type': 'application/json',
     }
-    url = 'https://api-stg.truststamp.net/api/v2/proxy/pad/face/image'
+    url = 'https://api-stg.truststamp.net/api/v2/proxy/pad/combined'
     myobj = {'media_url': "http://aayez.com:888" + image,
              "UUID": str(uuid.uuid4()),
              "return_now": True}
